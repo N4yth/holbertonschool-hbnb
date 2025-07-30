175cf056d753f16a3de3392d239a1dd151f1970b
N4yth <nathan.dupuis31@gmail.com>
2025-07-30 17:14:40 +0200

    feat : remove useless function move the prive filter and add commentary

--- part4/app/front/js/scripts_index.js
+++ part4/app/front/js/scripts_index.js
@@ -1,31 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
   checkAuthentication();
+
+  // prepare the price filter event
+  document.getElementById('price-filter').addEventListener('change', (event) => {
+    const placeCard = document.getElementsByClassName('place-card');
+    for (let i = 0; i < placeCard.length; i++) {
+      // test the save data in html tag with the value of the target elemnet value
+      if (Number(placeCard[i].dataset.price) >= Number(event.target.value)) {
+        placeCard[i].style.display = 'none';
+      } else {
+        placeCard[i].style.display = 'flex';
+      }
+    }
+  });
 });
 
-async function checkAuthentication() {
+async function checkAuthentication () {
+  // check if the current user have authenticiete himself
   const loginButton = document.getElementById('login-button');
   const logoutButton = document.getElementById('logout-button');
   const token = getCookie('token');
+  // check if the token is null or not
   if (!token) {
-    loginButton.style.display = 'block';
+    // the user is not authenticated
+    loginButton.style.display = 'flex';
     logoutButton.style.display = 'none';
   } else {
+    // the user is authenticated
     loginButton.style.display = 'none';
-    logoutButton.style.display = 'block';
-    // Fetch places data if the user is authenticated
-    document.getElementById("connected").textContent=getCookie("user")
+    logoutButton.style.display = 'flex';
+    document.getElementById('connected').textContent = getCookie('user');
     fetchPlaces(token);
   }
 }
 
-async function logOut() {
-  if (confirm("Are you sure to logout ?")) {
+async function logOut () {
+  // remove the user and token cookie and return to the main page
+  if (confirm('Are you sure to logout ?')) {
     suprCookie(['token', 'user']);
-    window.location.href='index.html';
+    window.location.href = 'index.html';
   }
 }
 
-function getCookie(name) {
+function getCookie (name) {
+  // get cookie by name
   const cookies = {};
   document.cookie.split('; ').forEach(cookie => {
     const [key, value] = cookie.split('=');
@@ -34,43 +52,45 @@ function getCookie(name) {
   return cookies[name];
 }
 
-function suprCookie (names){
+function suprCookie (names) {
+  // supre the cookie by name
   names.forEach(name => {
-    document.cookie = name+'=; Max-Age=-99999999;';
+    document.cookie = name + '=; Max-Age=-99999999;';
   });
 }
 
-async function fetchPlaces(token) {
-  // Make a GET request to fetch places data
-  // Include the token in the Authorization header
-  // Handle the response and pass the data to displayPlaces function
+async function fetchPlaces (token) {
+  // fetch place data
   const response = await fetch('http://127.0.0.1:5000/api/v1/places/', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
-    },
+      Authorization: `Bearer ${token}`
+    }
   });
   if (response.ok) {
-    
     const data = await response.json();
-    displayPlaces(data)
+    // data has been return and now display it
+    displayPlaces(data);
   } else {
-    alert('Login failed: ' + response.statusText);
+    const msg = await response.json();
+    alert('Login failed: ' + msg.error);
   }
 }
 
-function displayPlaces(places) {
+function displayPlaces (places) {
+  // display all places that as been create
   const placeCard = document.getElementById('places-list');
   places.forEach(place => {
     const article = document.createElement('article');
     const button = document.createElement('button');
     const title = document.createElement('h3');
     const price = document.createElement('p');
-    article.dataset.price = `${place.price}`
+    article.dataset.price = `${place.price}`;
     button.classList.add('details-button');
     button.classList.add('button');
-    button.addEventListener('click', function(){
+    // save the button that will save the place id to the token and change page
+    button.addEventListener('click', function () {
       document.cookie = `place=${place.id}; path=/`;
       window.location.href = 'place.html';
     });
@@ -86,28 +106,3 @@ function displayPlaces(places) {
     placeCard.appendChild(article);
   });
 }
-
-async function goLogin() {
-  window.location.href = 'login.html';
-}
-
-async function goIndex() {
-  window.location.href = 'index.html';
-}
-
-document.getElementById('price-filter').addEventListener('change', (event) => {
-  placeCard = document.getElementsByClassName('place-card')
-  for (let i = 0; i < placeCard.length; i++) {
-    if (Number(placeCard[i].dataset.price) >= Number(event.target.value)) {
-      placeCard[i].style.display = 'none';
-    }
-    else {
-      placeCard[i].style.display = 'flex';
-    }
-  }
-});
-
-function getPlaceId(test) {
-  console.log(test)
-}
-
