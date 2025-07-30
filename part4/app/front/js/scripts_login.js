418cbdb6ab6afbf2dcb9c4b8b389b0770362181f
N4yth <nathan.dupuis31@gmail.com>
2025-07-30 17:15:11 +0200

    feat : add commentary to the code and how to login

--- part4/app/front/js/scripts_login.js
+++ part4/app/front/js/scripts_login.js
@@ -1,15 +1,24 @@
+/* ------ How to login as a register user ------
+in the top right header you have a login button after press it you will be
+send to the login page you need to put your email and password and press
+"submit" to login and the site will automaticelly send you to theindex page
+if you have wrong user password or email only a error message will be display
+on the screen
+*/
 
-function getLogData() {
+function getLogData () {
+  // prepare the submit event
   const loginForm = document.getElementById('login-form');
   if (loginForm) {
     loginForm.addEventListener('submit', async (event) => {
       event.preventDefault();
-      loginUser(document.getElementById('email').value, document.getElementById('password').value)
+      loginUser(document.getElementById('email').value, document.getElementById('password').value);
     });
   }
 }
 
-async function loginUser(email, password) {
+async function loginUser (email, password) {
+  // fetch the login user data
   const response = await fetch('http://127.0.0.1:5000/api/v1/auth/login', {
     method: 'POST',
     headers: {
@@ -23,6 +32,6 @@ async function loginUser(email, password) {
     document.cookie = `user=${data.user_name}; path=/`;
     window.location.href = 'index.html';
   } else {
-    alert('Login failed: ' + response.statusText);
+    alert('Login failed: ' + response.json().error);
   }
 }
